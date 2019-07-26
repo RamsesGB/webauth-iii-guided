b2e2dfe64bd8a7f34441638dd73a991bc0fa738f
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-07-26 12:16:23 -0400

    guided project complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -25,8 +25,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // produce a token
+        const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -37,4 +41,17 @@ router.post('/login', (req, res) => {
     });
 });
 
+function generateToken(user) {
+  const jwtPayload = {
+    subject: user.id,
+    username: user.username,
+  }
+
+  const jwtSecret = process.env.JWT_SECRET || 'keep it secret, keep it safe!';
+
+  const jwtOptions = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
 module.exports = router;
